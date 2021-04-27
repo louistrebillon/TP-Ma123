bacb688709d5152b0007536b15d3d3b2e51e7db0
louistrebillon <81464790+louistrebillon@users.noreply.github.com>
2021-04-27 14:51:38 +0200

    Update TP4geniemathdichotomie.py

--- TP4geniemathdichotomie.py
+++ TP4geniemathdichotomie.py
@@ -11,21 +11,26 @@ def f(x):
     s1=x*exp(x)-7 #Modifier la fonction ici
     return(s1)
 
-def dicho(x):
+def dicho():
     a=float(input("Quel est x0 "))
     b=float(input("Quel est x1 "))
     err=float(input("Quelle est l'erreur? "))
     nitermax=float(input("Maximum d'itération ? "))
     
+    listxn=[]
+    listen=[]
     i=0
     m=0
     while abs(b-a) > err and i < nitermax:
+        listen.append(abs(b-a))
         m=(a+b)/2
         if (f(a) * f(m) <= 0) :
             b = m
+            listxn.append(b)
         else :
             a = m
+            listxn.append(a)
         i=i+1
-    return(a,i)
+    return(a,i,listxn,listen)
     
-print(dicho(0))
+print(dicho())
