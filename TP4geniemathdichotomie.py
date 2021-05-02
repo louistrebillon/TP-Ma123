fc4e3f7f82ebf4d4a2f1dbde799bba79b4cfbdfe
louistrebillon <81464790+louistrebillon@users.noreply.github.com>
2021-05-02 14:32:01 +0200

    Update TP4geniemathdichotomie.py

--- TP4geniemathdichotomie.py
+++ TP4geniemathdichotomie.py
@@ -6,9 +6,10 @@ Created on Mon Mar  1 10:34:29 2021
 """
 
 from math import exp
+import matplotlib.pyplot as p
 
 def f(x):
-    s1=x*exp(x)-7 #Modifier la fonction ici
+    s1=exp(x)*x-7 #Modifier la fonction ici
     return(s1)
 
 def dicho():
@@ -17,12 +18,13 @@ def dicho():
     err=float(input("Quelle est l'erreur? "))
     nitermax=float(input("Maximum d'itération ? "))
     
+    listi=[]
     listxn=[]
     listen=[]
     i=0
     m=0
     while abs(b-a) > err and i < nitermax:
-        listen.append(abs(b-a))
+        listen.append(float(abs(b-a)))
         m=(a+b)/2
         if (f(a) * f(m) <= 0) :
             b = m
@@ -31,6 +33,12 @@ def dicho():
             a = m
             listxn.append(a)
         i=i+1
+        listi.append(float(i))
+        
+    p.figure(figsize=(5,5), dpi=100)
+    p.semilogy(listi, listen)
+    p.savefig("En=f(n)")
+    p.show()
     return(a,i,listxn,listen)
     
 print(dicho())
